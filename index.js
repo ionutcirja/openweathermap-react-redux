2466ff19e27f110bc1b851faeef10d94c145448d
Ionut Cirja <ionut.cirja@gmail.com>
2017-02-14 16:01:08 +0000

    render forecast component

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { initStore } from './store';
+import { getWeatherData } from './actions/weather';
 import style from './sass/index.scss'; // eslint-disable-line no-unused-vars
+import Forecast from './containers/forecast';
 
 const store = initStore({});
+store.dispatch(getWeatherData());
 
 render(
-	<Provider store={store} />,
+	<Provider store={store}>
+		<Forecast />
+	</Provider>,
 	document.getElementById('root')
 );
