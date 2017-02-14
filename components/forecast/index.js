98ead684db0fbf612f54dbaf5cc2bbb4e9fb77a7
Ionut Cirja <ionut.cirja@gmail.com>
2017-02-14 17:25:49 +0000

    main forecast component

--- components/forecast/index.js
+++ components/forecast/index.js
@@ -1,16 +1,33 @@
 import React, { PropTypes } from 'react';
+import ForecastDay from '../forecast-day';
 
 const Forecast = props =>
 	<section className="forecast">
 		<h1>{props.cityName}</h1>
+		<ul className="forecast-days-list">
+			{
+				Object.keys(props.list).length > 0 &&
+				Object.keys(props.list).map(key =>
+					(
+						<ForecastDay
+							key={key}
+							dayName={key}
+							hoursList={props.list[key]}
+						/>
+					)
+				)
+			}
+		</ul>
 	</section>;
 
 Forecast.propTypes = {
-	cityName: PropTypes.string.isRequired
+	cityName: PropTypes.string.isRequired,
+	list: PropTypes.object.isRequired
 };
 
 Forecast.defaultProps = {
-	cityName: ''
+	cityName: '',
+	list: {}
 };
 
 export default Forecast;
