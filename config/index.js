689f4282c5cad0a0b67dff39663df3dad54bc196
Ionut Cirja <ionut.cirja@gmail.com>
2017-02-14 16:00:15 +0000

    add api key

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 const config = {
-	API_BASE_URL: 'http://api.openweathermap.org/data/2.5/weather'
+	API_BASE_URL: 'http://api.openweathermap.org/data/2.5/forecast',
+	API_KEY: '8e763fa1104a97a0bccbb3845532f8b7'
 };
 
 export default config;
