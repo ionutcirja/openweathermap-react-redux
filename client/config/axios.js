ac4ca191cba3225afc2f4fd9da932ef4c082e1bf
Ionut Cirja <ionut.cirja@gmail.com>
2019-03-14 10:51:04 +0000

    create client config

---
+++ client/config/axios.js
@@ -0,0 +1,10 @@
+// @flow
+import axios from 'axios';
+
+type Options = {
+  baseUrl: string,
+};
+
+export default ({ baseUrl }: Options) => {
+  axios.defaults.baseURL = baseUrl;
+};
---
+++ client/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  API_BASE_URL: 'http://api.openweathermap.org/data/2.5',
+  API_KEY: '8e763fa1104a97a0bccbb3845532f8b7',
+};
+
+export default config;
