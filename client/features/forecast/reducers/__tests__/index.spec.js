dd93673cba4374c28032fec454e32d6aa0726e52
Ionut Cirja <ionut.cirja@gmail.com>
2019-03-14 16:39:29 +0000

    update reducers tests

--- client/features/forecast/reducers/__tests__/index.spec.js
+++ client/features/forecast/reducers/__tests__/index.spec.js
@@ -1,26 +1,47 @@
-import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
 import reducer from '..';
 
-describe('Weather reducer', () => {
+describe('Forecast reducers', () => {
 	it('should return the default state if the action type is not defined', () => {
 		const initialState = { someProp: 'some value' };
 		deepFreeze(initialState);
-		expect(reducer(initialState, { type: 'UNDEFINED_ACTION' })).to.eql(initialState);
+		expect(reducer(initialState, { type: 'UNDEFINED_ACTION' })).toEqual(initialState);
 	});
 
-	it('should return a new state containing the forecast data if the action type is WEATHER_UPDATED', () => {
-		const initialState = {
-			loaded: false
-		};
-		const data = {
-			info: 'some info',
-			loaded: true
-		};
-		deepFreeze(initialState);
-		expect(reducer(initialState, { type: 'WEATHER_UPDATED', data })).to.eql({
-			info: 'some info',
-			loaded: true
-		});
-	});
+	it('should return a new state containing a true loading prop and a false error prop'
+    + ' if the action type is FORECAST_REQUEST', () => {
+    const initialState = {
+      someProp: 'some value',
+    };
+    deepFreeze(initialState);
+    expect(reducer(initialState, { type: 'FORECAST_REQUEST' })).toEqual({
+      loading: true,
+      error: false,
+    });
+  });
+
+  it('should return a new state containing a false loading prop and the payload content'
+    + ' if the action type is FORECAST_RESPONSE', () => {
+    const initialState = {
+      error: true,
+    };
+    const payload = { list: ['1', '2', '3'] };
+    deepFreeze(initialState);
+    expect(reducer(initialState, { type: 'FORECAST_RESPONSE', payload })).toEqual({
+      loading: false,
+      ...payload,
+    });
+  });
+
+  it('should return a new state containing a false loading prop and a true error prop'
+    + ' if the action type is FORECAST_ERROR', () => {
+    const initialState = {
+      someProp: 'some value',
+    };
+    deepFreeze(initialState);
+    expect(reducer(initialState, { type: 'FORECAST_ERROR' })).toEqual({
+      loading: false,
+      error: true,
+    });
+  });
 });
