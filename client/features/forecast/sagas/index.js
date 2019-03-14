19708ba1eabb799ea17a726bfca933119bc75a1b
Ionut Cirja <ionut.cirja@gmail.com>
2019-03-14 11:21:09 +0000

    create forecast sagas

---
+++ client/features/forecast/sagas/index.js
@@ -0,0 +1,24 @@
+// @flow
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { fetchForecast } from '../services';
+import type { Action } from '../../../types';
+import {
+  FORECAST_REQUEST,
+  forecastResponse,
+  forecastError,
+} from '../actions';
+
+export function* requestForecast(action: Action): Generator<*, *, *> {
+  const { location } = action.payload;
+  try {
+    const response = yield call(fetchForecast, location);
+    const { city, list } = response.data;
+    yield put(forecastResponse({ city, list }));
+  } catch (error) {
+    yield put(forecastError(error));
+  }
+}
+
+export function* watchForecastRequest(): Generator<*, *, *> {
+  yield takeLatest(FORECAST_REQUEST, requestForecast);
+}
